b76867a771ed2dba30e42eb0c1b3af0d54d949a7
twixx sturgill <twixxcustoms@gmail.com>
2019-05-08 17:02:02 -0400

    Day 3

--- 04-functions/05-arrowFunctions.js
+++ 04-functions/05-arrowFunctions.js
@@ -11,3 +11,13 @@ let coffee = () => {
 }
 coffee();
 
+//Consise body
+//Consise bodies don't need parenthesis unless you have more than one parameter     vvv
+//Return is implied
+//Line breaks are bad
+let apples = (x,y) => console.log(`There are ${x} apples and ${y} pears.`);        //One parameter does not need parenthesis
+apples(10,5)
+
+//Block body
+let apples = (x) => {  console.log(`There are ${x} apples.`); }
+apples(10);
